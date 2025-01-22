9c63a93e9ae6579205cb9f1e2113766c0320722e
Golam Rafi <golamrafi27oo@gmail.com>
2025-01-23 00:13:39 +0600

    bug fix

--- source/main.js
+++ source/main.js
@@ -5,5 +5,15 @@ const toggleNav = () => {
   navbar.classList.toggle("active");
   mobileNav.classList.toggle("hamburger-active");
 };
-mobileNav.addEventListener("click", () => toggleNav());
 
+mobileNav.addEventListener("click", (event) => {
+  event.stopPropagation(); // Prevent click from propagating to document
+  toggleNav();
+});
+
+document.addEventListener("click", (event) => {
+  if (!navbar.contains(event.target) && !mobileNav.contains(event.target)) {
+    navbar.classList.remove("active");
+    mobileNav.classList.remove("hamburger-active");
+  }
+});
